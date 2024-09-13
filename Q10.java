fe59fbd76d873dbd2a9088fbf3296264796e7bae
IvK1fs <166817912+IvK1fs@users.noreply.github.com>
2024-09-13 09:06:37 -0300

    Create Q10.java

---
+++ Q10.java
@@ -0,0 +1,31 @@
+package application;
+
+
+
+import java.util.Scanner;
+
+public class main {
+
+	public static void main(String[] args) {
+		
+	
+	
+	Scanner sc = new Scanner(System.in);
+	
+	System.out.print("Digite a quantidade de kilometros: "); 
+	
+	Double km = sc.nextDouble();
+	
+	System.out.println("Digite a quantidade de dias: ");
+	
+	Double dias = sc.nextDouble();
+	
+	
+	
+System.out.printf("O valor a ser cobrado será: R$%.2f", (90 * dias)+(km * 0.20) );
+
+	
+        sc.close();
+	}
+
+}

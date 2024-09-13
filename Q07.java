8784f68b0f54754ed9f1211f2c438cb5e9f9f5ce
IvK1fs <166817912+IvK1fs@users.noreply.github.com>
2024-09-13 08:42:53 -0300

    Create Q07.java

---
+++ Q07.java
@@ -0,0 +1,24 @@
+package application;
+
+
+import java.util.Scanner;
+
+public class main {
+
+	public static void main(String[] args) {
+		
+	
+	
+	Scanner sc = new Scanner(System.in);
+	
+	System.out.print("Digite a distância em metros: "); //
+	
+	Double num = sc.nextDouble();
+	
+System.out.printf("a distância %f coreresponde a%n%.5fKm%n%.5fHm%n%.5fDam%n%.3fdm%n%.3fcm%n%.3fmm ", num, num/1000, num / 100,num / 10, num * 10, num * 100, num * 1000);
+
+	
+        sc.close();
+	}
+
+}

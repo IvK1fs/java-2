8d78ff172863405c1d817d676684174e02ae8f1e
IvK1fs <166817912+IvK1fs@users.noreply.github.com>
2024-09-13 08:30:25 -0300

    Create Q06.java

---
+++ Q06.java
@@ -0,0 +1,24 @@
+package application;
+
+
+import java.util.Scanner;
+
+public class main {
+
+	public static void main(String[] args) {
+		
+	
+	
+	Scanner sc = new Scanner(System.in);
+	
+	System.out.print("Digite um número: "); //utilize virgula;
+	
+	Double num = sc.nextDouble();
+	
+System.out.printf("O dobro de %.2f é %.2f%nA terça parte de %.2f é %.4f",num, num *2, num, num/3 );
+
+	
+        sc.close();
+	}
+
+}

7de06da59f329e94b9f992334c8b84d1ad6ecea6
IvK1fs <166817912+IvK1fs@users.noreply.github.com>
2024-09-13 09:26:39 -0300

    Create Q12.java

---
+++ Q12.java
@@ -0,0 +1,18 @@
+package application;
+
+
+public class main {
+
+	public static void main(String[] args) {
+		
+	int calc = (9 + 8 + 7) /3  ;
+	int calc2 = (4 + 5 + 6) / 3;
+	int calc3 =  calc + calc2; 
+	int calc4 = calc + calc2 / 2;
+System.out.printf("A media aritmetica  dos valores 9,8,7 é %d%nA media de 4+5+6 é igual a %d%nA soma das medias é igual a %d%n a media das medias é %d",calc,calc2, calc3, calc4 );
+
+	
+        
+	}
+
+}

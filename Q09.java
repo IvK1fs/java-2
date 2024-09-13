cce7ad75330d4e102a29c9f2a915b6e371b6a23c
IvK1fs <166817912+IvK1fs@users.noreply.github.com>
2024-09-13 10:01:25 -0300

    Update Q09.java

--- Q09.java
+++ Q09.java
@@ -4,7 +4,7 @@ import java.util.Locale;
 
 import java.util.Scanner;
 
-public class main {
+public class Main {
 
 	public static void main(String[] args) {
 		Locale.setDefault(Locale.US); //utilize ponto no imput;

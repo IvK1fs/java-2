fb78d925f080a806fad47404339577162a1ed630
IvK1fs <166817912+IvK1fs@users.noreply.github.com>
2024-09-13 07:55:39 -0300

    Create Q01.java

---
+++ Q01.java
@@ -0,0 +1,22 @@
+package application;
+
+
+import java.util.Scanner;
+
+public class main {
+
+	public static void main(String[] args) {
+		
+	
+	
+	Scanner sc = new Scanner(System.in);
+	
+	System.out.print("Insira o seu nome: ");
+	String nome = sc.nextLine();
+	
+System.out.printf("Olá %s, é um prazer de te conhecer!",nome);
+	
+        sc.close();
+	}
+
+}
